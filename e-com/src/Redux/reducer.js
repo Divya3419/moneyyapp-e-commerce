047d4e6e6a5835bdabed7173749994463ff3be2b
Divya <99958902+Divya3419@users.noreply.github.com>
2022-08-30 22:08:44 +0530

    done

---
+++ e-com/src/Redux/reducer.js
@@ -0,0 +1,13 @@
+let initialState ={
+    product:[],
+    isLoading:false,
+    isError:false,
+
+}
+
+export const reducer=(state=initialState,{type,payload})=>{
+
+    switch(type){
+        default
+    }
+}
