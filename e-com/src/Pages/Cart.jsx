9a017b5fdbba4ebed7934f7e6fa49bbe21def715
Divya <99958902+Divya3419@users.noreply.github.com>
2022-09-01 10:33:35 +0530

    done

--- e-com/src/Pages/Cart.jsx
+++ e-com/src/Pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "./Common.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getCart } from "../Redux/action";
+import axios from "axios";
 const Cart = () => {
   const data = useSelector((state) => state.item);
   const dispatch = useDispatch();
@@ -22,6 +23,11 @@ const Cart = () => {
                 <p>Brand : {ele.brand}</p>
                 <p>Product Name : {ele.name}</p>
                 <p>Price : {ele.price}</p>
+                <button onClick={()=>{
+                  axios.delete(`https://blueproduct.herokuapp.com/cartItem/${ele.id}`).then(()=>{
+                    dispatch(getCart(data));
+                  })
+                }} >Delete</button>
               </div>
             </div>
           );
